240cc4f85c3bc7ad80cfc50c20d2898e8f87e1ff
galyverasi <galyverasi@gmail.com>
2021-11-23 07:34:32 -0700

    fixed express typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const methodOverride = require('method-override')
 
-const app = expressS()
+const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
